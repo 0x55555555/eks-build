08f8f825f6f964b58f8a13d193dde883eaddcf59
Jorj <jorjpimm@gmail.com>
2014-02-16 15:01:14 +0000

    clang dependency

--- BuildAndTestDebian.sh
+++ BuildAndTestDebian.sh
@@ -2,7 +2,7 @@
 
 qbs setup-toolchains --detect
 qbs setup-qt /usr/bin/qmake qt
-qbs config profiles.qt.baseProfile `qbs config defaultProfile`
+qbs config profiles.qt.baseProfile clang
 qbs profile:qt $EKS_BUILD_VARIANT
 
 
