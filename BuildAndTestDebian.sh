0943695a9c40e48a5970a3cd257761d31fc5e7aa
Jorj <jorjpimm@gmail.com>
2014-02-16 14:55:57 +0000

    fix

--- BuildAndTestDebian.sh
+++ BuildAndTestDebian.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-qbs setup-toolchains
+qbs setup-toolchains --detect
 qbs setup-qt /usr/bin/qmake qt
 qbs config profiles.qt.baseProfile `qbs config defaultProfile`
 qbs profile:qt $EKS_BUILD_VARIANT
